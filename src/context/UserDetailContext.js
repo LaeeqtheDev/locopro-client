87952975e3a863b32cfea3074081ce30946900d3
Adeel525 <111305261+Adeel525@users.noreply.github.com>
2024-06-26 18:47:00 +0500

    Update UserDetailContext.js

--- src/context/UserDetailContext.js
+++ src/context/UserDetailContext.js
@@ -1,5 +1,1 @@
-import {createContext} from 'react'
-
-const UserDetailContext = createContext()
-
-export default UserDetailContext
+import{createContext}from 'react' const UserDetailContext=createContext() export default UserDetailContext
