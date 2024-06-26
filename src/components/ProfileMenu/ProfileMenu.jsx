179a6a2217237402b1b4b83606dcb08a2f7e2c02
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2024-06-26 12:47:41 -0400

    Update ProfileMenu.jsx

--- src/components/ProfileMenu/ProfileMenu.jsx
+++ src/components/ProfileMenu/ProfileMenu.jsx
@@ -1,2 +1,37 @@
-import React from 'react';import{Dropdown}from 'react-bootstrap';import 'bootstrap/dist/css/bootstrap.min.css';import{useNavigate}from 'react-router-dom';const ProfileMenu=({user,logout})=>{const navigate=useNavigate()
-return(<Dropdown><Dropdown.Toggle variant="light" id="dropdown-basic"><img src={user?.image}alt="user" className="rounded-circle" style={{width:'40px',height:'40px'}}/></Dropdown.Toggle><Dropdown.Menu><Dropdown.Item onClick={()=>navigate("./favourites",{replace:!0})}>Favourites</Dropdown.Item><Dropdown.Item onClick={()=>navigate("./bookings",{replace:!0})}>Bookings</Dropdown.Item><Dropdown.Item onClick={()=>{localStorage.clear();logout()}}>Logout</Dropdown.Item></Dropdown.Menu></Dropdown>)};export default ProfileMenu
+import React from 'react';
+import { Dropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useNavigate } from 'react-router-dom';
+
+const ProfileMenu = ({ user, logout }) => {
+  const navigate = useNavigate();
+
+  const handleFavouritesClick = () => {
+    navigate("/favourites", { replace: true });
+  };
+
+  const handleBookingsClick = () => {
+    navigate("/bookings", { replace: true });
+  };
+
+  const handleLogoutClick = () => {
+    localStorage.clear(); // Clear local storage or any other necessary cleanup
+    logout(); // Call logout function provided by Auth0 or your authentication service
+  };
+
+  return (
+    <Dropdown>
+      <Dropdown.Toggle variant="light" id="dropdown-basic">
+        <img src={user?.picture} alt="user" className="rounded-circle" style={{ width: '40px', height: '40px' }} />
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={handleFavouritesClick}>Favourites</Dropdown.Item>
+        <Dropdown.Item onClick={handleBookingsClick}>Bookings</Dropdown.Item>
+        <Dropdown.Item onClick={handleLogoutClick}>Logout</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
+
+export default ProfileMenu;
