ec19ce10adb3b77c166deacb1b87559784e85d05
Adeel525 <111305261+Adeel525@users.noreply.github.com>
2024-06-26 19:13:43 +0500

    Update Website.jsx

--- src/pages/Website.jsx
+++ src/pages/Website.jsx
@@ -1,30 +1,3 @@
 import React from 'react'
-import Companies from "../components/Companies/Companies";
-import Contact from "../components/Contact/Contact";
-import Footer from "../components/Footer/Footer";
-import GetStarted from "../components/GetStarted/GetStarted";
-import Header from "../components/Header/Header";
-import Hero from "../components/Hero/Hero";
-import Residencies from "../components/Residencies/Residencies";
-import Value from "../components/Value/Value";
-import PriceCal from '../components/PriceCal/PriceCal';
-
-const Website = () => {
-  return (
-    <div className="App">
-    <div>
-      <div className="white-gradient" />
-      <Hero />
-    </div>
-    <Companies />
-    <Residencies/>
-    <Value/>
-    <Contact/>  
-    <PriceCal/>
-    
-    <GetStarted/>
-  </div>
-  )
-}
-
+import Companies from "../components/Companies/Companies";import Contact from "../components/Contact/Contact";import Footer from "../components/Footer/Footer";import GetStarted from "../components/GetStarted/GetStarted";import Header from "../components/Header/Header";import Hero from "../components/Hero/Hero";import Residencies from "../components/Residencies/Residencies";import Value from "../components/Value/Value";import PriceCal from '../components/PriceCal/PriceCal';const Website=()=>{return(<div className="App"><div><div className="white-gradient"/><Hero/></div><Companies/><Residencies/><Value/><Contact/><PriceCal/><GetStarted/></div>)}
 export default Website
