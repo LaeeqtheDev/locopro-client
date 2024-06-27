11e342056d980ecd775fc39190388a6c5ae22aa0
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2024-06-27 07:08:56 -0400

    Update Map.jsx

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import {MapContainer, TileLayer} from 'react-leaflet'
 import GeoCoderMarker from '../GeoCoderMarker/GeoCoderMarker'
-
-const Map = ({ address, city, country }) => {
+const Map = ({address, city, country}) => {
   return (
     <MapContainer
-      center={[31.5497, 74.3436]}  // Coordinates for Lahore
-      zoom={12}  // Set a reasonable zoom level for city view
-      scrollWheelZoom={true}
-      style={{
+    center={[53.35, 18.8]}
+    zoom={1}
+    scrollWheelZoom={false}
+    style={{
         height: "40vh",
         width: "100%",
         marginTop: "20px",
         zIndex: 0,
-      }}
+       
+    }}
     >
-      <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'/>
-      <GeoCoderMarker address={`${address} ${city} ${country}`} />
+        <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'/>
+        <GeoCoderMarker address={`${address} ${city} ${country}`} />
     </MapContainer>
   )
 }
