82a531a71f408cd5777b7a98780f751b20b8c017
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2024-06-27 06:13:22 -0400

    Update Map.jsx

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,45 @@
-import React from 'react';import{MapContainer,TileLayer}from 'react-leaflet';import GeoCoderMarker from '../GeoCoderMarker/GeoCoderMarker';const Map=({address,city,country})=>{return(<MapContainer
-center={[53.35,18.8]}
-zoom={1}
-scrollWheelZoom={!1}
-style={{height:'40vh',width:'100%',marginTop:'20px',zIndex:0,}}><TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'/><GeoCoderMarker address={`${address} ${city} ${country}`}/></MapContainer>)};export default Map
+import React, { useEffect } from 'react';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+
+const UpdateMapView = ({ address, city, country }) => {
+  const map = useMap();
+
+  useEffect(() => {
+    const geocodeAddress = async () => {
+      const fullAddress = `${address}, ${city}, ${country}`;
+      const response = await fetch(
+        `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(fullAddress)}&format=json`
+      );
+      const results = await response.json();
+      if (results.length > 0) {
+        const { lat, lon } = results[0];
+        map.setView([lat, lon], 13);
+      }
+    };
+
+    geocodeAddress();
+  }, [address, city, country, map]);
+
+  return null;
+};
+
+const Map = ({ address, city, country }) => {
+  return (
+    <MapContainer
+      center={[53.35, 18.8]}
+      zoom={1}
+      scrollWheelZoom={false}
+      style={{
+        height: '40vh',
+        width: '100%',
+        marginTop: '20px',
+        zIndex: 0,
+      }}
+    >
+      <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+      <UpdateMapView address={address} city={city} country={country} />
+    </MapContainer>
+  );
+};
+
+export default Map;
