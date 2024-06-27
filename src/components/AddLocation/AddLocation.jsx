ec48353beeb7d3cc883d5aef411e88c00a6d2ede
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2024-06-27 07:03:37 -0400

    Update AddLocation.jsx

--- src/components/AddLocation/AddLocation.jsx
+++ src/components/AddLocation/AddLocation.jsx
@@ -1,5 +1,4 @@
-// AddLocation.jsx
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 import useCountries from "../../hooks/useCountries";
@@ -7,13 +6,29 @@ import Map from "../Map/Map";
 
 const AddLocation = ({ propertyDetails, setPropertyDetails, nextStep }) => {
   const { getAll } = useCountries();
-  const { register, handleSubmit, formState: { errors } } = useForm({
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({
     defaultValues: {
       country: propertyDetails?.country || "Pakistan",
       city: propertyDetails?.city || "Lahore",
       address: propertyDetails?.address || "Punjab, Lahore",
     }
   });
+
+  const [location, setLocation] = useState({
+    country: propertyDetails?.country || "Pakistan",
+    city: propertyDetails?.city || "Lahore",
+    address: propertyDetails?.address || "Punjab, Lahore",
+  });
+
+  const watchedFields = watch(["country", "city"]);
+
+  useEffect(() => {
+    setLocation({
+      ...location,
+      country: watchedFields.country,
+      city: watchedFields.city,
+    });
+  }, [watchedFields.country, watchedFields.city]);
 
   const onSubmit = (data) => {
     setPropertyDetails({
@@ -65,7 +80,7 @@ const AddLocation = ({ propertyDetails, setPropertyDetails, nextStep }) => {
 
         {/* Right side - Map */}
         <div style={{ flex: 1 }}>
-          <Map country={propertyDetails?.country} city={propertyDetails?.city} address={propertyDetails?.address} />
+          <Map country={location.country} city={location.city} address={propertyDetails?.address} />
         </div>
       </div>
 
