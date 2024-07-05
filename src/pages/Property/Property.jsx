e12536cdab130baf1dbcb217254dce0c46953779
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2024-07-05 13:28:00 -0400

    Update Property.jsx

--- src/pages/Property/Property.jsx
+++ src/pages/Property/Property.jsx
@@ -149,7 +149,7 @@ const Property = () => {
                   validateLogin() && setModalOpened(true);
                 }}
               >
-                Book your visit
+                Book your Inspection
               </button>
             )}
 
